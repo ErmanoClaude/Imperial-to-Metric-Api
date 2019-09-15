5eac5d871b862b5693a9f6b600993f1fcc0755ca
Glitch (feline-kitten) <none>
2019-09-15 19:09:56 +0000

    🌊🎬 Checkpoint ./server.js:4818891/88

--- server.js
+++ server.js
@@ -4,11 +4,10 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
-
+var helmet      = require('helmet');
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
-
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -17,6 +16,9 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//HelmetJs
+app.use(helmet());
 
 //Index page (static HTML)
 app.route('/')
