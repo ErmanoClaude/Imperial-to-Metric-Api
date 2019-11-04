b90e6748a7ec79a5f937efc4dea8185254f89663
Glitch (feline-kitten) <none>
2019-11-04 09:02:12 +0000

    🏙🖖 Checkpoint ./tests/2_functional-tests.js:4818891/701

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -38,19 +38,32 @@ suite('Functional Tests', function() {
         .get('/api/convert')
         .query({input:'32g'})
         .end(function(err,res){
-          assert.equal(res.status,200)
+          assert.equal(res.status,200);
+          assert.equal(res.body.error, "invalid unit");
         })
         done();
       });
       
       test('Convert 3/7.2/4kg (invalid number)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input:'3/7.2/4kg'})
+        .end(function(err,res){
+          assert.equal(res.status,200)
+          assert.equal(res.body.error, "invalid number")
+        })
+        done();
       });  
       
       test('Convert 3/7.2/4kilomegagram (invalid number and unit)', function(done) {
-        
-        //done();
+        chai.request(server)
+        .get('/api/convert')
+        .query({input:'3/7.2/4kilomegagram'})
+        .end(function(err, res){
+          assert.equal(res.status, 200);
+          assert.equal(res.body.error, "invalid number and unit")
+        })
+        done();
       });
       
       test('Convert kg (no number)', function(done) {
